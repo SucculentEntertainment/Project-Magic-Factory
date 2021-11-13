3dd4020d8af7502a2bd6abb73f5fc7564db4f388
BitTim <tim.anhalt.2004@protonmail.com>
2021-11-13 15:23:46 +0100

    Started on Player Controller (Accidentally put into BaseController)

--- Assets/Scripts/Entity/BaseEntity.cs
+++ Assets/Scripts/Entity/BaseEntity.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BaseEntity : MonoBehaviour
 {
+    private string state = "idle";
+    
     [Header("Properties")]
     public string typeID;
     public BaseController controller;
@@ -13,14 +15,28 @@ public class BaseEntity : MonoBehaviour
     public string instanceID;
     public string typeName;
 
-    void Start()
+
+
+    private void Start()
     {
         if(instanceID == "") instanceID = InstanceManager.createInstance(this);
         InstanceManager.logInstances();
     }
 
-    void Update()
+    private void Update()
     {
+        foreach(BaseBehaviour b in behaviours)
+        {
+            if(b.targetState == state) b.exec();
+        }
+    }
+
+
+
+    public void setState(string state, string returnState = "")
+    {
+
         
+        this.state = state;
     }
 }
--- Assets/Scripts/Entity/Controller/BaseController.cs
+++ Assets/Scripts/Entity/Controller/BaseController.cs
@@ -6,11 +6,27 @@ public class BaseController : MonoBehaviour
 {
     void Start()
     {
-        
+         
     }
 
     void Update()
     {
         
+    }
+    
+    private void OnMove(InputValue dirVal)
+    {
+        if (disableDirectional) return;
+        movDir = dirVal.Get<Vector2>();
+
+        if(movDir.magnitude > 0) inputData.setData("move", moveDir, aimDir);
+        else inputData.setData("idle", moveDir, aimDir);
+    }
+
+    private void OnAim(InputValue posVal)
+    {
+        Vector2 worldPos = mainCamera.ScreenToWorldPoint(posVal.Get<Vector2>());
+        mouseDir = worldPos - gm.playerPosition;
+        mouseDir.Normalize();
     }
 }
